9915d442eb135c5316d5a9ab34923ba01536d6c4
Matheus Altrao <matheusaltrao2@gmail.com>
2026-01-16 18:00:35 -0400

    feat(ui): update Perfil component to handle user authentication and sign-out functionality

--- components/header/perfil.tsx
+++ components/header/perfil.tsx
@@ -1,10 +1,14 @@
+import { signOutAction } from "@/actions/auth/sign-out-action";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { auth } from "@/lib/auth";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
 import CreditQuantityCard from "./credit-quantity-card";
 import GoogleSigninButton from "./google-signin-button";
 
-export default function Perfil() {
-  const hasSession = true;
+export default async function Perfil() {
+  const session = await auth();
+  const hasSession = session && session.user;
 
   return (
     <div>
@@ -18,8 +22,8 @@ export default function Perfil() {
             <DropdownMenu>
               <DropdownMenuTrigger className="cursor-pointer" asChild>
                 <Avatar>
-                  <AvatarImage src="https://github.com/shadcn.png" />
-                  <AvatarFallback>CN</AvatarFallback>
+                  <AvatarImage src={hasSession.image || ""} />
+                  <AvatarFallback>{hasSession.name?.[0]}</AvatarFallback>
                 </Avatar>
               </DropdownMenuTrigger>
               <DropdownMenuContent>
@@ -30,7 +34,11 @@ export default function Perfil() {
 
                 <DropdownMenuItem>Outras Análises</DropdownMenuItem>
                 <DropdownMenuItem>Comprar Créditos</DropdownMenuItem>
-                <DropdownMenuItem>Sair</DropdownMenuItem>
+                <form action={signOutAction}>
+                  <Button variant={"destructive"} className="w-full h-8 justify-start" type="submit">
+                    Sair
+                  </Button>
+                </form>
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
